26c7caf0f819e66c8fd3da9e2a1767200be160c1
Marcus Bramsen <k4wkv49@gmail.com>
2025-09-10 10:59:00 +0200

    Ship structure with checks of Kill, Hit or Miss.

--- src/BattleshipGame/Ship.java
+++ src/BattleshipGame/Ship.java
@@ -1,4 +1,24 @@
 package BattleshipGame;
 
 public class Ship {
+    private int[] locationCells;
+    private int numOfHits;
+
+    public void setLocation(int[] locs) {
+        locationCells = locs;
+    }
+
+    public String checkHit(int guess) {
+        for (int cell : locationCells) {
+            if (cell == guess) {
+                numOfHits++;
+                if (numOfHits == locationCells.length) {
+                    return "Kill!";
+                }
+            } else {
+                return "Hit!";
+            }
+        }
+        return "Miss!";
+    }
 }
