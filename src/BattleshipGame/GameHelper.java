d38ef8788fd5dc83e899be26c0bb035bf8f69abb
trist <trho0001@stud.ek.dk>
2025-09-12 15:05:40 +0200

    uiguhljk

--- src/BattleshipGame/GameHelper.java
+++ src/BattleshipGame/GameHelper.java
@@ -1,4 +1,14 @@
 package BattleshipGame;
 
+import java.util.Scanner;
+
 public class GameHelper {
+    Scanner sc = new Scanner(System.in);
+
+    public String getUserInput() {
+        String userInput;
+        userInput = sc.nextLine();
+        return userInput;
+
+    }
 }
